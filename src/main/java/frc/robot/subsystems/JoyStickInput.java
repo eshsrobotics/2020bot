23cef7f5c8943a95a1a37d049eb5886617f19824
uakotaobi <uche.robotics@akotaobi.org>
2020-01-31 15:40:24 -0800

    vhmjvgkmhvgmjvn

---
+++ src/main/java/frc/robot/subsystems/JoyStickInput.java
@@ -0,0 +1,34 @@
+package frc.robot.subsystems;
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class JoyStickInput extends SubsystemBase {
+
+    public final double L = 29;
+    public final double W = 16;
+
+    public void drive (double x1, double y1, double x2){
+        double r = Math.sqrt ((L * L) + (W * W));
+        y1 *= -1;
+    
+        double a = x1 - x2 * (L / r);
+        double b = x1 + x2 * (L / r);
+        double c = y1 - x2 * (W / r);
+        double d = y1 + x2 * (W / r);
+
+        double backRightSpeed = Math.sqrt ((a * a) + (d * d));
+        double backLeftSpeed = Math.sqrt ((a * a) + (c * c));
+        double frontRightSpeed = Math.sqrt ((b * b) + (d * d));
+        double frontLeftSpeed = Math.sqrt ((b * b) + (c * c));
+    
+        double backRightAngle = Math.atan2 (a, d) / Math.PI;
+        double backLeftAngle = Math.atan2 (a, c) / Math.PI;
+        double frontRightAngle = Math.atan2 (b, d) / Math.PI;
+        double frontLeftAngle = Math.atan2 (b, c) / Math.PI;
+    }
+}
