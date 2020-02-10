3e35cdab954534eb2ce49e965fc42737ca7f1a18
uakotaobi <uche.github@akotaobi.org>
2020-02-10 04:08:43 -0800

    Saving current work.
    I'm in the middle of a hairy OpenCV dependency build on my system, but that
    might not be needed if I can pull my current changes onto another machine that
    builds successfully.
    
    Why is it always OpenCV?

--- src/main/java/frc/robot/subsystems/WheelDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/WheelDriveSubsystem.java
@@ -8,6 +8,10 @@ package frc.robot.subsystems;
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
+import java.util.List;
+import java.util.Collections;
+import java.util.ArrayList;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkMax;
@@ -16,13 +20,14 @@ import frc.robot.Constants;
 
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.PWMSparkMax;
+import edu.wpi.first.wpilibj.PWMSpeedController;
 import edu.wpi.first.wpilibj.controller.PIDController;
 
 /**
  * This class is designed to take inputs from OI and JoystickInput, and control
  * the driving motors for driving based on those inputs. It will handle all
  * modes of driving, such as CLASSIC and SNAKE.
- * 
+ *
  * For all arrays of 4 in this class, the indices within the arrays are
  * indicated by Constants.FRONT_RIGHT, Constants.FRONT_LEFT,
  * Constants.BACK_LEFT, and Constants.BACK_RIGHT.
@@ -35,7 +40,7 @@ public class WheelDriveSubsystem extends SubsystemBase {
     /**
      * This array contains four elements, one for each wheel.  It maintains the
      * last values passed into setGoalAngles().
-     * 
+     *
      * These are directional angles, with 0 radians indicating straight forward and
      * positive angles turning counterclockwise.
      */
@@ -47,13 +52,38 @@ public class WheelDriveSubsystem extends SubsystemBase {
      */
     private double[] initialEncoderValues;
 
-    public WheelDriveSubsystem(int angleMotor, int speedMotor, int encoder) {
-        this.angleMotor = new PWMSparkMax(angleMotor);
-        this.speedMotor = new PWMSparkMax(speedMotor);
-        this.pidController = new PIDController(1, 0, 0);
+    /**
+     * This array contains four PWM-driven SpeedControllers, one for each
+     * wheel.
+     *
+     * We use this abstract class on purpose: any motor controllers than can
+     * be driven by PWM signals can drive the wheels forward, not just the
+     * Spark MAX.  Normal Sparks and CIM motors will work just as well!
+     */
+    private List<PWMSpeedController> speedMotors;
+
+    public WheelDriveSubsystem(int frontLeftPwmPort, int backLeftPwmPort,
+                               int frontRightPwmPort, int backRightPwmPort) {
+
+    //public WheelDriveSubsystem(int angleMotor, int speedMotor, int encoder) {
+        //this.angleMotor = new PWMSparkMax(angleMotor);
+        //this.speedMotor = new PWMSparkMax(speedMotor);
+        //this.pidController = new PIDController(1, 0, 0);
 
         this.goalThetas = new double[4];
         this.initialEncoderValues = new double[4];
+
+        // Fill the speedMotors list with 4 nulls and then overwrite them.
+        //
+        // By doing things this way, we make this code immune to changes in
+        // the values of the indexing constants (FRONT_LEFT, FRONT_RIGHT, and
+        // so on.)
+        this.speedMotors = new List<PWMSpeedController>();
+        Collections.addAll(this.speedMotors, new SpeedController[] { null, null, null, null });
+        this.speedMotors.set(Constants.FRONT_LEFT, new PWMSparkMax(frontLeftPwmPort));
+        this.speedMotors.set(Constants.FRONT_RIGHT, new PWMSparkMax(frontRightPwmPort));
+        this.speedMotors.set(Constants.BACK_LEFT, new PWMSparkMax(backLeftPwmPort));
+        this.speedMotors.set(Constants.BACK_RIGHT, new PWMSparkMax(backRightPwmPort));
 
         pidController.setIntegratorRange(-1.0, 1.0);
         pidController.enableContinuousInput(-1.0, 1.0);
@@ -79,14 +109,14 @@ public class WheelDriveSubsystem extends SubsystemBase {
     /**
      * Sets the desired directional angles for the drives.  "Directional angles" are relative to a bearing of 0
      * degrees, which for each wheel of the robot is assumed to point directly forward at the start of robotInit().
-     * 
+     *
      * Ultimately, the goal of the input system is to set the appropriate goal angles for each drive mode,
      * and the goal of the WheelDriveSubsystem itself is to move the wheels until they align with these goals.
-     * 
+     *
      * All angles are in radians.
-     * 
-     * @param thetas An array of exactly 4 desired direction values.  The 
-     *               indices are as specified in 
+     *
+     * @param thetas An array of exactly 4 desired direction values.  The
+     *               indices are as specified in
      *               Constants.{FRONT,BACK}_{LEFT,RIGHT}.
      */
     public void setGoalAngles(double[] thetas) {
@@ -99,9 +129,9 @@ public class WheelDriveSubsystem extends SubsystemBase {
     /**
      * Measures the four existing encoder values for the four wheels and
      * presumes that they are all pointing directly to the front.
-     * 
+     *
      * Thereafter, all pivoting angles will be relative to these initial measurements.
-     * 
+     *
      * It is recommended to call calibrate() once at the very start of the match, during robotInit().
      */
     public void calibrate() {
@@ -121,12 +151,12 @@ public class WheelDriveSubsystem extends SubsystemBase {
     }
 
     /**
-     * 
-     * @param turningRadius which is in meters.   
+     *
+     * @param turningRadius which is in meters.
      * @return An array of four directional angles.
-     * 
-     *         All angles are given in radians, in respect to the starting position of the wheels. The starting angle is pi/2 radians. 
-     *         To find the absolutel angle of the wheel, it would be pi/2 + the directional angle. 
+     *
+     *         All angles are given in radians, in respect to the starting position of the wheels. The starting angle is pi/2 radians.
+     *         To find the absolutel angle of the wheel, it would be pi/2 + the directional angle.
      *         The convention is that counterclockwise pivot angles are positive, while clockwise pivot angles are negative.
      */
     public static double[] snakeDriveGetAngle(double turningRadius) {
