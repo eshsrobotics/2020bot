9c13347d0a80e02f5c9771c8c2293f24bbca2d9e
the-cooler-jackson <shark2016mellon@gmail.com>
2021-02-14 19:16:29 -0800

    Started on the Trajectory timer
    Started implementing a timer that would start once a trajectory is
    placed. This is for the 4-wheeled subsystem.

--- src/main/java/frc/robot/subsystems/NewWheelDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/NewWheelDriveSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
@@ -10,6 +11,7 @@ public class NewWheelDriveSubsystem extends SubsystemBase {
     
     private Trajectory autonomousTrajectory;
     private SwerveDriveKinematics kinematics;
+    private Timer trajectoryTimer; 
 
     /**
      * Initializes an instance of this class!?
@@ -23,14 +25,27 @@ public class NewWheelDriveSubsystem extends SubsystemBase {
                                                new Translation2d(-h, -v),  // BACK_LEFT
                                                new Translation2d(+h, -v),  // BACK_RIGHT
                                                new Translation2d(+h, +v)); // FRONT_RIGHT
+        trajectoryTimer = null;
     }
+
+    /**
+     *This function allows this object to set a timer whenever it recieves a trajectory
+     * @param newTrajectory The trajectory to set.
+     */
+    public void setTrajectory(Trajectory newTrajectory) {
+        this.autonomousTrajectory = newTrajectory;
+        this.trajectoryTimer = new Timer();
+        this.trajectoryTimer.start();
+    }
+
     /**
      * Instantaneous updates to the drive.
      */
     @Override 
     public void periodic() {
         if (autonomousTrajectory != null) {
-            
+            Trajectory.State state = autonomousTrajectory.sample(trajectoryTimer.get());
         }
     }
+
 }
